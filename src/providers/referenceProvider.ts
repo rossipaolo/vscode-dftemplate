85ff85bf829978707d272f0e8bb1c52952d3d992
TheLacus <TheLacus@users.noreply.github.com>
2018-08-19 13:39:41 +0200

    Refactored reference provider

--- src/providers/referenceProvider.ts
+++ src/providers/referenceProvider.ts
@@ -11,36 +11,36 @@ import { Modules } from '../language/modules';
 
 export class TemplateReferenceProvider implements ReferenceProvider {
 
+    private static queries = (document: TextDocument, word: string) => [
+        {
+            accept: () => parser.findSymbolDefinition(document, word) !== undefined,
+            do: (includeDeclaration: boolean) => parser.findSymbolReferences(document, word, includeDeclaration)
+        },
+        {
+            accept: () => parser.findTaskDefinition(document, word) !== undefined,
+            do: (includeDeclaration: boolean) => parser.findTasksReferences(document, word, includeDeclaration)
+        },
+        {
+            accept: () => parser.findMessageDefinition(document, word) !== undefined,
+            do: (includeDeclaration: boolean) => parser.findMessageReferences(document, word, includeDeclaration)
+        }
+    ]
+
     public provideReferences(document: TextDocument, position: Position, options: { includeDeclaration: boolean }, token: CancellationToken): Thenable<Location[]> {
         return new Promise(function (resolve, reject) {
             const line = document.lineAt(position.line);
             const word = parser.getWord(document, position);
             if (word) {
 
-                // Symbol
-                if (parser.findSymbolDefinition(document, word)) {
+                const query = TemplateReferenceProvider.queries(document, word).find(x => x.accept());
+                if (query) {
                     const locations: Location[] = [];
-                    for (const range of parser.findSymbolReferences(document, word, options.includeDeclaration)) {
+                    for (const range of query.do(options.includeDeclaration)) {
                         locations.push(new Location(document.uri, range));
                     }
                     return resolve(locations);
                 }
 
-                // Task
-                if (parser.findTaskDefinition(document, word)) {
-                    const locations: Location[] = [];
-                    for (const range of parser.findTasksReferences(document, word, options.includeDeclaration)) {
-                        locations.push(new Location(document.uri, range));
-                    }
-                    return resolve(locations);
-                }
-
-                // Message
-                const messages = Array.from(parser.findMessageReferences(document, word, options.includeDeclaration));
-                if (messages.length > 0) {
-                    return resolve(messages.map(x => new Location(document.uri, x)));
-                }
-                
                 // Quest
                 if (parser.isQuestReference(line.text)) {
                     return parser.findQuestReferences(word, token).then((locations) => resolve(locations), () => reject());
