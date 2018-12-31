8058d79e85a6b720d8d9702b608f51f756ecac4a
TheLacus <24359151+TheLacus@users.noreply.github.com>
2018-12-31 19:07:46 +0100

    Moved quest parsing to Quest class

--- src/language/quest.ts
+++ src/language/quest.ts
@@ -5,9 +5,16 @@
 'use strict';
 
 import * as vscode from 'vscode';
+import * as parser from '../parsers/parser';
 import { Preamble } from './preamble';
 import { Qbn } from './qbn';
 import { Qrc } from './qrc';
+
+enum QuestBlock {
+    Preamble,
+    QRC,
+    QBN
+}
 
 /**
  * A quest that corresponds to a text file.
@@ -30,6 +37,43 @@ export class Quest {
     public readonly qbn = new Qbn();
 
     public constructor(public readonly document: vscode.TextDocument) {
+        
+        let block = QuestBlock.Preamble;
+        this.preamble.found = true;
+
+        for (let index = 0; index < this.document.lineCount; index++) {
+            const line = this.document.lineAt(index);
+    
+            // Skip comments and empty lines
+            if (parser.isEmptyOrComment(line.text)) {
+                continue;
+            }
+    
+            // Detect next block
+            if (line.text.indexOf('QRC:') !== -1) {
+                block = QuestBlock.QRC;
+                this.qrc.found = true;
+                continue;
+            }
+            else if (line.text.indexOf('QBN:') !== -1) {
+                block = QuestBlock.QBN;
+                this.qbn.found = true;
+                continue;
+            }
+    
+            // Parse current block
+            switch (block) {
+                case QuestBlock.Preamble:
+                    this.preamble.parse(line);
+                    break;
+                case QuestBlock.QRC:
+                    this.qrc.parse(this.document, line);
+                    break;
+                case QuestBlock.QBN:
+                    this.qbn.parse(line);
+                    break;
+            }
+        }
     }
 
     public getLocation(range: vscode.Range): vscode.Location {
