63be2162ef047178c477837470ec7992369cbdb8
TheLacus <24359151+TheLacus@users.noreply.github.com>
2019-03-27 21:00:47 +0100

    Refactored and improved CompletionItemProvider

--- src/providers/completionItemProvider.ts
+++ src/providers/completionItemProvider.ts
@@ -6,7 +6,7 @@
 
 import * as vscode from 'vscode';
 import { TextDocument, Position, CompletionItem, CancellationToken } from 'vscode';
-import { QuestResourceCategory, SymbolType } from '../language/static/common';
+import { QuestResourceCategory, SymbolType, QuestResourceInfo } from '../language/static/common';
 import { Modules } from '../language/static/modules';
 import { Language } from '../language/static/language';
 import { Tables } from '../language/static/tables';
@@ -17,153 +17,188 @@ export class TemplateCompletionItemProvider implements vscode.CompletionItemProv
 
     private static readonly signatureInfoCommand = { command: 'editor.action.triggerParameterHints', title: '' };
 
-    public provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Thenable<CompletionItem[]> {
-        return new Promise(function (resolve, reject) {
-            const line = document.lineAt(position.line);
-            const text = line.text.substring(0, position.character - 2).trim();
-            const prefix = TemplateCompletionItemProvider.getPrefix(line.text, position.character);
+    public async provideCompletionItems(document: TextDocument, position: Position, token: CancellationToken): Promise<CompletionItem[] | undefined> {
+        const line = document.lineAt(position.line);
+        const text = line.text.substring(0, position.character - 2).trim();
+        const prefix = TemplateCompletionItemProvider.getPrefix(line.text, position.character);
+        const quest = Quest.get(document);
 
-            // Find %abc symbols
-            if (line.text[position.character - 2] === '%') {
-                const items: CompletionItem[] = [];
-                for (const symbol of Language.getInstance().findSymbols('%' + prefix)) {
-                    let item = new vscode.CompletionItem(symbol.details.signature, vscode.CompletionItemKind.Property);
-                    item.detail = symbol.details.signature;
-                    item.documentation = symbol.details.summary;
+        if (quest.preamble.range && quest.preamble.range.contains(position)) {
+            return TemplateCompletionItemProvider.preambleCompletionItems(prefix);
+        } else if (quest.qrc.range && quest.qrc.range.contains(position)) {
+            return TemplateCompletionItemProvider.qrcCompletionItems(quest, position, line, prefix);
+        } else if (quest.qbn.range && quest.qbn.range.contains(position)) {
+            return TemplateCompletionItemProvider.qbnCompletionItems(quest, line, text, prefix, token);
+        }
+    }
+
+    private static preambleCompletionItems(prefix: string): CompletionItem[] {
+        const items: CompletionItem[] = [];
+
+        // Directives
+        for (const directive of Language.getInstance().findKeywords(prefix)) {
+            items.push(TemplateCompletionItemProvider.signatureCompletionItem(directive));
+        }
+
+        return items;
+    }
+
+    private static qrcCompletionItems(quest: Quest, position: Position, line: vscode.TextLine, prefix: string): CompletionItem[] {
+        const items: CompletionItem[] = [];
+
+        if (line.text[position.character - 2] === '%') {
+            // Context macros
+            for (const symbol of Language.getInstance().findSymbols('%' + prefix)) {
+                const item = new vscode.CompletionItem(symbol.details.signature, vscode.CompletionItemKind.Property);
+                item.detail = symbol.details.signature;
+                item.documentation = symbol.details.summary;
+                items.push(item);
+            }
+        } else {
+            // Symbols
+            for (const symbol of quest.qbn.iterateSymbols()) {
+                Language.getInstance().getSymbolVariations(symbol.name, symbol.type).forEach(variation => {
+                    const item = new vscode.CompletionItem(variation.word, vscode.CompletionItemKind.Field);
+                    item.detail = `${symbol.line.text.trim()} (${variation.description})`;
                     items.push(item);
-                }
-                return resolve(items);
+                });
             }
 
+            // Messages
+            if ('message'.startsWith(prefix.toLowerCase())) {
+                const item = new vscode.CompletionItem('Message: id', TemplateCompletionItemProvider.getCompletionItemKind(QuestResourceCategory.Message));
+                item.insertText = new vscode.SnippetString('Message:  ${1:' + quest.qrc.getAvailableId(position) + '}');
+                item.detail = 'Message: id';
+                item.documentation = 'An additional message block';
+                items.push(item);
+            }
 
-            const items: CompletionItem[] = [];
-            const quest = Quest.get(document);
+            // Directives and static messages
+            for (const directive of [
+                ...Language.getInstance().findKeywords(prefix),
+                ...Language.getInstance().findMessages(prefix)]) {
+                items.push(TemplateCompletionItemProvider.signatureCompletionItem(directive));
+            }
+        }
 
-            const param = TemplateCompletionItemProvider.findParamSignature(line, prefix, text);
-            if (param) {
+        return items;
+    }
 
-                // Inside an invocation: suggests values according to parameter type
-                switch (param) {
-                    case ParameterTypes.questName:
-                        return TemplateCompletionItemProvider.findQuestsCompletionItems(prefix, token).then((items) => {
-                            return resolve(items);
-                        }), () => reject();
-                    case ParameterTypes.message:
-                    case ParameterTypes.messageName:
-                        for (const message of quest.qrc.messages) {
-                            if (message.alias && message.alias.startsWith(prefix)) {
-                                const item = new CompletionItem(message.alias, vscode.CompletionItemKind.Struct);
-                                item.detail = document.lineAt(message.range.start.line).text.trim();
-                                items.push(item);
-                            }
-                        }
-                        break;
-                    case ParameterTypes.symbol:
-                        TemplateCompletionItemProvider.doSymbolSuggestions(items, quest, prefix);
-                        break;
-                    case ParameterTypes.clockSymbol:
-                        TemplateCompletionItemProvider.doSymbolSuggestions(items, quest, prefix, SymbolType.Clock);
-                        break;
-                    case ParameterTypes.foeSymbol:
-                        TemplateCompletionItemProvider.doSymbolSuggestions(items, quest, prefix, SymbolType.Foe);
-                        break;
-                    case ParameterTypes.itemSymbol:
-                        TemplateCompletionItemProvider.doSymbolSuggestions(items, quest, prefix, SymbolType.Item);
-                        break;
-                    case ParameterTypes.personSymbol:
-                        TemplateCompletionItemProvider.doSymbolSuggestions(items, quest, prefix, SymbolType.Person);
-                        break;
-                    case ParameterTypes.placeSymbol:
-                        TemplateCompletionItemProvider.doSymbolSuggestions(items, quest, prefix, SymbolType.Place);
-                        break;
-                    case ParameterTypes.task:
-                        for (const task of quest.qbn.iterateTasks()) {
-                            const kind = task.isVariable ? vscode.CompletionItemKind.Variable : vscode.CompletionItemKind.Method;
-                            const item = new vscode.CompletionItem(task.definition.symbol, kind);
-                            item.detail = document.lineAt(task.range.start.line).text.trim();
+    private static async qbnCompletionItems(quest: Quest, line: vscode.TextLine, text: string, prefix: string, token: CancellationToken): Promise<CompletionItem[]> {
+        const items: CompletionItem[] = [];
+
+        const param = TemplateCompletionItemProvider.findParamSignature(line, prefix, text);
+        if (param) {
+            // Inside an invocation: suggests values according to parameter type
+            switch (param) {
+                case ParameterTypes.questName:
+                    const upperCasePrefix = prefix.toUpperCase();
+                    const quests = await Quest.getAll(token);
+                    quests.forEach(quest => {
+                        const name = quest.getName();
+                        if (name && name.toUpperCase().startsWith(upperCasePrefix)) {
+                            const item = new CompletionItem(name, vscode.CompletionItemKind.Class);
+                            item.detail = quest.preamble.getDisplayName() || name;
                             items.push(item);
                         }
-                        break;
-                    case ParameterTypes.effectKey:
-                        for (const effectKey of Modules.getInstance().getEffectKeys(prefix)) {
-                            items.push(new vscode.CompletionItem(effectKey, vscode.CompletionItemKind.EnumMember));
+                    });
+                case ParameterTypes.message:
+                case ParameterTypes.messageName:
+                    for (const message of quest.qrc.messages) {
+                        if (message.alias && message.alias.startsWith(prefix)) {
+                            const item = new CompletionItem(message.alias, vscode.CompletionItemKind.Struct);
+                            item.detail = quest.document.lineAt(message.range.start.line).text.trim();
+                            items.push(item);
                         }
-                        break;
-                    default:
-                        TemplateCompletionItemProvider.doParamSuggestions(items, param);
-                        break;
-                }
-
-            }
-            else {
-
-                // Empty string: suggests invocation of definition/action/condition
-                if (text.length === 0) {
-                    TemplateCompletionItemProvider.findSignatures(prefix, items);
-
-                    if ('message'.startsWith(prefix.toLowerCase())) {
-                        items.push(TemplateCompletionItemProvider.messageDefinition(quest, position));
                     }
-                }
-
-                // Find all symbols defined in the quest
-                for (const symbol of quest.qbn.iterateSymbols()) {
-                    const item = new vscode.CompletionItem(symbol.name, vscode.CompletionItemKind.Field);
-                    item.detail = symbol.line.text;
+                    break;
+                case ParameterTypes.symbol:
+                    TemplateCompletionItemProvider.symbolCompletionItems(items, quest, prefix);
+                    break;
+                case ParameterTypes.clockSymbol:
+                    TemplateCompletionItemProvider.symbolCompletionItems(items, quest, prefix, SymbolType.Clock);
+                    break;
+                case ParameterTypes.foeSymbol:
+                    TemplateCompletionItemProvider.symbolCompletionItems(items, quest, prefix, SymbolType.Foe);
+                    break;
+                case ParameterTypes.itemSymbol:
+                    TemplateCompletionItemProvider.symbolCompletionItems(items, quest, prefix, SymbolType.Item);
+                    break;
+                case ParameterTypes.personSymbol:
+                    TemplateCompletionItemProvider.symbolCompletionItems(items, quest, prefix, SymbolType.Person);
+                    break;
+                case ParameterTypes.placeSymbol:
+                    TemplateCompletionItemProvider.symbolCompletionItems(items, quest, prefix, SymbolType.Place);
+                    break;
+                case ParameterTypes.task:
+                    for (const task of quest.qbn.iterateTasks()) {
+                        const kind = task.isVariable ? vscode.CompletionItemKind.Variable : vscode.CompletionItemKind.Method;
+                        const item = new vscode.CompletionItem(task.definition.symbol, kind);
+                        item.detail = quest.document.lineAt(task.range.start.line).text.trim();
+                        items.push(item);
+                    }
+                    break;
+                case ParameterTypes.effectKey:
+                    for (const effectKey of Modules.getInstance().getEffectKeys(prefix)) {
+                        items.push(new vscode.CompletionItem(effectKey, vscode.CompletionItemKind.EnumMember));
+                    }
+                    break;
+                default:
+                    const suggestions = Tables.getInstance().getValues(param);
+                    if (suggestions) {
+                        suggestions.forEach(suggestion => {
+                            items.push(new vscode.CompletionItem(suggestion, vscode.CompletionItemKind.EnumMember));
+                        });
+                    }
+                    break;
+            }
+        } else {
+            // Actions/condition
+            for (const result of Modules.getInstance().findActions(prefix)) {
+                for (const overload of result.details.overloads) {
+                    const signature = Modules.prettySignature(overload);
+                    const item = new vscode.CompletionItem(signature, TemplateCompletionItemProvider.getCompletionItemKind(result.category));
+                    item.insertText = new vscode.SnippetString(overload);
+                    item.detail = result.moduleName + ' -> ' + signature;
+                    item.documentation = new vscode.MarkdownString(result.details.summary);
+                    item.command = TemplateCompletionItemProvider.signatureInfoCommand;
                     items.push(item);
                 }
             }
 
-            return items.length > 0 ? resolve(items) : reject();
-        });
-    }
-
-    private static async findQuestsCompletionItems(prefix: string, token: CancellationToken): Promise<CompletionItem[]> {
-        prefix = prefix.toUpperCase();
-        const quests = await Quest.getAll(token);
-        return quests.reduce((items, quest) => {
-            const name = quest.getName();
-            if (name && name.toUpperCase().startsWith(prefix)) {
-                const item = new CompletionItem(name, vscode.CompletionItemKind.Class);
-                item.detail = quest.preamble.getDisplayName() || name;
-                items.push(item);
-            }
-            return items;
-        }, new Array<CompletionItem>());
-    }
-
-    private static findSignatures(prefix: string, items: vscode.CompletionItem[]) {
-        // Language
-        for (const languageItem of Language.getInstance().seekByPrefix(prefix)) {
-            let prettySignature = Language.prettySignature(languageItem.details.signature);
-            let item = new vscode.CompletionItem(prettySignature, TemplateCompletionItemProvider.getCompletionItemKind(languageItem.category));
-            item.insertText = new vscode.SnippetString(Language.getInstance().getSymbolSnippet(languageItem.details.signature));
-            item.detail = prettySignature;
-            item.documentation = new vscode.MarkdownString(languageItem.details.summary);
-            item.command = TemplateCompletionItemProvider.signatureInfoCommand;
-            items.push(item);
-        }
-
-        // Modules
-        for (const result of Modules.getInstance().findActions(prefix)) {
-            for (const overload of result.details.overloads) {
-                let signature = Modules.prettySignature(overload);
-                let item = new vscode.CompletionItem(signature, TemplateCompletionItemProvider.getCompletionItemKind(result.category));
-                item.insertText = new vscode.SnippetString(overload);
-                item.detail = result.moduleName + ' -> ' + signature;
-                item.documentation = new vscode.MarkdownString(result.details.summary);
-                item.command = TemplateCompletionItemProvider.signatureInfoCommand;
-                items.push(item);
+            // Other signatures
+            for (const resourceInfo of [
+                ...Language.getInstance().findKeywords(prefix),
+                ...Language.getInstance().findDefinitions(prefix),
+                ...Language.getInstance().findGlobalVariables(prefix)]) {
+                items.push(TemplateCompletionItemProvider.signatureCompletionItem(resourceInfo));
             }
         }
+
+        return items;
     }
 
-    private static messageDefinition(quest: Quest, position: Position): CompletionItem {
-        const item = new vscode.CompletionItem('Message: id', TemplateCompletionItemProvider.getCompletionItemKind(QuestResourceCategory.Message));
-        item.insertText = new vscode.SnippetString('Message:  ${1:' + quest.qrc.getAvailableId(position) + '}');
-        item.detail = 'Message: id';
-        item.documentation = 'An additional message block';
+    private static signatureCompletionItem(resourceInfo: QuestResourceInfo): CompletionItem {
+        const prettySignature = Language.prettySignature(resourceInfo.details.signature);
+        const item = new vscode.CompletionItem(prettySignature, TemplateCompletionItemProvider.getCompletionItemKind(resourceInfo.category));
+        const snippet = resourceInfo.category === QuestResourceCategory.Definition ?
+            Language.getInstance().getSymbolSnippet(resourceInfo.details.signature) :
+            resourceInfo.details.signature;
+        item.insertText = new vscode.SnippetString(snippet);
+        item.detail = prettySignature;
+        item.documentation = new vscode.MarkdownString(resourceInfo.details.summary);
+        item.command = TemplateCompletionItemProvider.signatureInfoCommand;
         return item;
+    }
+
+    private static symbolCompletionItems(items: vscode.CompletionItem[], quest: Quest, prefix: string, type?: string) {
+        for (const symbol of quest.qbn.iterateSymbols()) {
+            if ((!type || symbol.type === type) && symbol.name.startsWith(prefix)) {
+                const item = new CompletionItem(symbol.name, vscode.CompletionItemKind.Field);
+                item.detail = symbol.line.text.trim();
+                items.push(item);
+            }
+        }
     }
 
     private static findParamSignature(line: vscode.TextLine, prefix: string, previousText: string): string | undefined {
@@ -189,25 +224,6 @@ export class TemplateCompletionItemProvider implements vscode.CompletionItemProv
         const actionResult = Modules.getInstance().findAction(line.text, match[1]);
         if (actionResult) {
             return Modules.getParameterAtPosition(actionResult, previousText.split(' ').length);
-        }
-    }
-
-    private static doParamSuggestions(items: vscode.CompletionItem[], signatureWord: string) {
-        const suggestions = Tables.getInstance().getValues(signatureWord);
-        if (suggestions) {
-            suggestions.forEach(suggestion => {
-                items.push(new vscode.CompletionItem(suggestion, vscode.CompletionItemKind.EnumMember));
-            });
-        }
-    }
-
-    private static doSymbolSuggestions(items: vscode.CompletionItem[], quest: Quest, prefix: string, type?: string) {
-        for (const symbol of quest.qbn.iterateSymbols()) {
-            if ((!type || symbol.type === type) && symbol.name.startsWith(prefix)) {
-                const item = new CompletionItem(symbol.name, vscode.CompletionItemKind.Field);
-                item.detail = symbol.line.text.trim();
-                items.push(item);
-            }
         }
     }
 
