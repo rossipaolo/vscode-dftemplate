924b0e017aedfc6a3946443f6d8e0ad79e0062b5
TheLacus <24359151+TheLacus@users.noreply.github.com>
2019-04-02 20:51:59 +0200

    Fixed quest rename

--- src/providers/renameProvider.ts
+++ src/providers/renameProvider.ts
@@ -5,51 +5,44 @@
 'use strict';
 
 import * as parser from '../parser';
-
 import { RenameProvider, TextDocument, Position, WorkspaceEdit, Range, CancellationToken } from 'vscode';
+import { Symbol } from '../language/common';
 import { Quest } from '../language/quest';
 import { TemplateReferenceProvider } from './referenceProvider';
-import { Symbol } from '../language/common';
 
 export class TemplateRenameProvider implements RenameProvider {
 
-    public provideRenameEdits(document: TextDocument, position: Position, newName: string, token: CancellationToken): Thenable<WorkspaceEdit> {
-        return new Promise(function (resolve, reject) {
-            const word = parser.getWord(document, position);
-            if (word) {
-                const edit = new WorkspaceEdit();             
-                
-                if (parser.isQuestReference(document.lineAt(position.line).text, word)) {
-                    return TemplateReferenceProvider.questReferences(word, true, token).then(locations => {
-                        for (const location of locations) {
-                            edit.replace(location.uri, location.range, newName);
-                        }
-                        return resolve(edit);
-                    });
-                } else {
-                    const quest = Quest.get(document);
+    public async provideRenameEdits(document: TextDocument, position: Position, newName: string, token: CancellationToken): Promise<WorkspaceEdit | undefined> {
+        const word = parser.getWord(document, position);
+        if (word) {
+            const edit = new WorkspaceEdit();
 
-                    const symbolOrTask = quest.qbn.getSymbol(word) || quest.qbn.getTask(word);
-                    if (symbolOrTask) {          
-                        const name = parser.symbols.getSymbolName(word);
-                        newName = parser.symbols.getSymbolName(newName);
+            if (parser.isQuestReference(document.lineAt(position.line).text, word)) {
+                for (const location of await TemplateReferenceProvider.questReferences(word, true, token)) {
+                    edit.replace(location.uri, location.range, newName);
+                }
+            } else {
+                const quest = Quest.get(document);
 
-                        function subRange(range: Range, text: string, subText: string): Range {
-                            const offset = text.indexOf(subText);
-                            return new Range(range.start.line, range.start.character + offset, range.start.line, range.start.character + offset + subText.length);
-                        }
+                const symbolOrTask = quest.qbn.getSymbol(word) || quest.qbn.getTask(word);
+                if (symbolOrTask) {
+                    const name = parser.symbols.getSymbolName(word);
+                    newName = parser.symbols.getSymbolName(newName);
 
-                        for (const location of symbolOrTask instanceof Symbol ?
-                            TemplateReferenceProvider.symbolReferences(quest, symbolOrTask, true) :
-                            TemplateReferenceProvider.taskReferences(quest, symbolOrTask, true)) {
-                            edit.replace(document.uri, subRange(location.range, document.getText(location.range), name), newName);
-                        }
-                        return resolve(edit);
+                    function subRange(range: Range, text: string, subText: string): Range {
+                        const offset = text.indexOf(subText);
+                        return new Range(range.start.line, range.start.character + offset, range.start.line, range.start.character + offset + subText.length);
+                    }
+
+                    for (const location of symbolOrTask instanceof Symbol ?
+                        TemplateReferenceProvider.symbolReferences(quest, symbolOrTask, true) :
+                        TemplateReferenceProvider.taskReferences(quest, symbolOrTask, true)) {
+                        edit.replace(document.uri, subRange(location.range, document.getText(location.range), name), newName);
                     }
                 }
             }
 
-            return reject();
-        });
+            return edit;
+        }
     }
 }
